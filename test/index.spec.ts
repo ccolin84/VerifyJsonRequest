851c9f9afe0bce66187324c015020e46d1104ce6
ccolin84 <ccolin84@gmail.com>
2018-07-17 17:41:32 -0400

    refactor tests, add mocks and start adding more tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,91 +2,108 @@ import { expect } from "chai";
 import "mocha";
 import * as sinon from "sinon";
 
-import { JsonValue, Next, Request, Response, verifyJsonRequest } from "../lib";
+import {
+  compareRequestToConfig,
+  JsonValue,
+  Next,
+  Request,
+  Response,
+  verifyJsonRequest,
+} from "../lib";
+
+import {
+  multipleMixedParams,
+  multipleObjectParams,
+  multipleValueParams,
+  singleObjectParam,
+  singleValueParam,
+} from "./mocks";
+
+describe("Compare Request To Config", function() {
+  it("not implimented");
+});
 
 describe("Verify Json Request", function() {
+  let request: Request;
+  let response: Response;
+  let responseStatusWatcher: sinon.SinonSpy;
+  let responseJsonWatcher: sinon.SinonSpy;
+  let next: sinon.SinonSpy;
+
+  beforeEach(function() {
+    request = { body: {} };
+    responseStatusWatcher = sinon.fake();
+    responseJsonWatcher = sinon.fake();
+    response = {
+      status(responseStatus) {
+        responseStatusWatcher(responseStatus);
+        return this;
+      },
+      json(responseJson) {
+        responseJsonWatcher(responseJson);
+        return this;
+      },
+    };
+    next = sinon.fake();
+  });
+
   it("Should return a middleware function", function() {
     const middlewareFn = verifyJsonRequest({ params: [] });
     expect(middlewareFn).to.be.a("function");
   });
 
-  it("Should return a middleware fn that calls its 'next' callback if json request is validated", function() {
+  it("Should call its 'next' callback if json request is validated", function() {
     // an empty array of params should always be validated
     const middlewareFn = verifyJsonRequest({ params: [] });
-    const request = { body: {} };
-    const responseStatusWatcher = sinon.fake();
-    const responseJsonWatcher = sinon.fake();
-    const response: Response = {
-      status(responsestatus) {
-        responseStatusWatcher();
-        return this;
-      },
-      json(responsejson) {
-        responseJsonWatcher();
-        return this;
-      },
-    };
-    const next = sinon.fake();
     middlewareFn(request, response, next);
     expect(next.calledOnce).to.equal(true);
     expect(responseStatusWatcher.calledOnce).to.equal(false);
     expect(responseJsonWatcher.calledOnce).to.equal(false);
   });
 
-  it("should call response.status and response.json on requests with invalid json bodies", function() {
+  it("Should not call its 'next' callback if json request is invalidated", function() {
+    // any empty body will should fail a non empty array of rules
     const middlewareFn = verifyJsonRequest({
-      params: [
-        {
-          errorMessage: "missing username",
-          expectedType: { type: "value", value: JsonValue.String },
-          key: "username",
-        },
-      ],
+      params: singleValueParam,
     });
-    const request = { body: {} };
-    const responseStatusWatcher = sinon.fake();
-    const responseJsonWatcher = sinon.fake();
-    const response: Response = {
-      status(responsestatus) {
-        responseStatusWatcher();
-        return this;
-      },
-      json(responsejson) {
-        responseJsonWatcher();
-        return this;
-      },
-    };
-    const next = sinon.fake();
     middlewareFn(request, response, next);
     expect(next.calledOnce).to.equal(false);
     expect(responseStatusWatcher.calledOnce).to.equal(true);
     expect(responseJsonWatcher.calledOnce).to.equal(true);
   });
 
-  it("Should accept requests with bodies that do match the input config", function() {
+  it("should call response.status and response.json on invalid requests", function() {
+    // an empty array of params should always be validated
     const middlewareFn = verifyJsonRequest({
-      params: [
-        {
-          errorMessage: "missing username",
-          expectedType: { type: "value", value: JsonValue.String },
-          key: "username",
-        },
-      ],
+      params: singleValueParam,
     });
-    const request = { body: { username: "John" } };
-    const responseStatusWatcher = sinon.fake();
-    const responseJsonWatcher = sinon.fake();
-    const response: Response = {
-      status(responsestatus) {
-        responseStatusWatcher();
-        return this;
-      },
-      json(responsejson) {
-        responseJsonWatcher();
-        return this;
-      },
-    };
-    const next = sinon.fake();
+    middlewareFn(request, response, next);
+    expect(next.calledOnce).to.equal(false);
+    expect(responseStatusWatcher.calledOnceWithExactly(400)).to.equal(true);
+    expect(
+      responseJsonWatcher.calledOnceWithExactly({
+        message: "missing username",
+        status: "fail",
+      }),
+    ).to.equal(true);
+  });
+
+  it("Should accept requests with single value bodies that match the input config", function() {
+    const middlewareFn = verifyJsonRequest({
+      params: singleValueParam,
+    });
+    request = { body: { username: "John" } };
+    middlewareFn(request, response, next);
+    expect(next.calledOnce).to.equal(true);
+    expect(responseStatusWatcher.calledOnce).to.equal(false);
+    expect(responseJsonWatcher.calledOnce).to.equal(false);
+  });
+
+  it("Should accept requests with single object bodies that match the input config", function() {
+    const middlewareFn = verifyJsonRequest({
+      params: singleObjectParam,
+    });
+    request = { body: { username: "John" } };
     middlewareFn(request, response, next);
     expect(next.calledOnce).to.equal(true);
     expect(responseStatusWatcher.calledOnce).to.equal(false);
