9bc66d95a6f927d5a900fe65a3174a80379147e9
ccolin84 <ccolin84@gmail.com>
2018-08-08 20:51:28 -0400

    break comparison logic for expected value type to provided value type into separate function from compareRequestToConfig

--- lib/verifyJsonRequest.ts
+++ lib/verifyJsonRequest.ts
@@ -11,11 +11,13 @@ export function verifyJsonRequest(config: Config): Middleware {
   if (!config) {
     throw new Error("No config object provided!");
   }
+
   return (request: Request, response: Response, next: Next): void => {
     const { isValid, errorMessage } = compareRequestToConfig(
       config.params,
       request.body,
     );
+
     if (!isValid) {
       sendError(response, errorMessage);
     } else {
@@ -25,32 +27,44 @@ export function verifyJsonRequest(config: Config): Middleware {
 }
 
 export function compareRequestToConfig(
-  params: RequestJsonKey[],
+  requiredParams: RequestJsonKey[],
   body: any,
 ): RequestConfigComparision {
   const comparison = { isValid: true, errorMessage: "" };
-  params.every(({ key, errorMessage, expectedType }) => {
-    let foundError: boolean = false;
-    const bodyKeyValue = body[key];
-    const bodyKeyType = typeof body[key];
 
-    if (!bodyKeyValue) {
-      foundError = true;
-    } else if (expectedType.type === "value") {
-      foundError = expectedType.value !== bodyKeyType;
-    } else if (expectedType.type === "object" && bodyKeyType === "object") {
-      foundError = !compareRequestToConfig(expectedType.value, bodyKeyValue)
-        .isValid;
-    }
+  requiredParams.every((requiredParam) => {
+    const foundError: boolean = requiredParamNotVerified(requiredParam, body);
 
     if (foundError) {
       comparison.isValid = false;
-      comparison.errorMessage = errorMessage;
+      comparison.errorMessage = requiredParam.errorMessage;
     }
 
     return !foundError;
   });
   return comparison;
+}
+
+export function requiredParamNotVerified(
+  requiredParam: RequestJsonKey,
+  body: any,
+): boolean {
+  const { expectedType, key } = requiredParam;
+  const providedValue = body[key];
+  const providedValueType = typeof body[key];
+
+  // verify a value has been provided
+  if (!providedValue) {
+    return true;
+  } else if (expectedType.type === "value") {
+    // verify it is the right type
+    return expectedType.value !== providedValueType;
+  } else if (expectedType.type === "object" && providedValueType === "object") {
+    // verify the object passed
+    return !compareRequestToConfig(expectedType.value, providedValue).isValid;
+  }
+
+  return false;
 }
 
 function sendError(response: Response, errorMessage: string): void {
